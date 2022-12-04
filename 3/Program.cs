c095c4be3572538332f096bb7f8bab3d2b86a098
Ildar <ildar.azmatov@ya.ru>
2022-12-04 22:16:33 +0500

    растояние между точками в квадрате

---
+++ 3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+//A (3,6); B (2,1) -> 5,09
+//A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите две точки А ");
+Console.WriteLine("Введите две точки B ");
+double xb = Convert.ToUInt32(Console.ReadLine());
+double xa = Convert.ToUInt32(Console.ReadLine());
+double yb = Convert.ToUInt32(Console.ReadLine());
+double ya = Convert.ToUInt32(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2));
+
+Console.Write(result);
