00407432944256e22fc072c2165c4a6e23db50b9
Ildar <ildar.azmatov@ya.ru>
2022-12-06 17:11:18 +0500

    Испаривил ошибку

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -4,12 +4,12 @@
 
 Console.WriteLine("Введите три точки А ");
 Console.WriteLine("Введите три точки B ");
-double xa = Convert.ToUInt64(Console.ReadLine());
-double xb = Convert.ToUInt64(Console.ReadLine());
-double ya = Convert.ToUInt64(Console.ReadLine());
-double yb = Convert.ToUInt64(Console.ReadLine());
-double ca = Convert.ToUInt64(Console.ReadLine());
-double cb = Convert.ToUInt64(Console.ReadLine());
+double xa = Convert.ToInt64(Console.ReadLine());
+double xb = Convert.ToInt64(Console.ReadLine());
+double ya = Convert.ToInt64(Console.ReadLine());
+double yb = Convert.ToInt64(Console.ReadLine());
+double ca = Convert.ToInt64(Console.ReadLine());
+double cb = Convert.ToInt64(Console.ReadLine());
 double result = Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2) + Math.Pow((cb - ca), 2));
 double Result = Math.Round(result , 2);
 
